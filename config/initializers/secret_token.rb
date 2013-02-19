38e940c383468be562f1fbcdf4d7912a94542181
vened <maxstbn@gmail.com>
2013-02-20 01:29:03 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shikocc::Application.config.secret_token = 'ee9992117bcb8df6e7c8f7e4498f8423ac14ff8e0e459ed39957064aa11aadf8d026b500c6de71e731c66dbcb4468dfa4999194cdd3d79d675e12a0ff0e379bb'
