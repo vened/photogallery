f4dbcf93ab73ed8dc3463154b303a2074d66db6f
vened <maxstbn@gmail.com>
2013-04-18 09:00:13 +0400

    session save db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416094217) do
+ActiveRecord::Schema.define(:version => 20130418045332) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -85,6 +85,26 @@ ActiveRecord::Schema.define(:version => 20130416094217) do
     t.decimal  "price"
     t.decimal  "old_price"
     t.decimal  "weight"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "text_pages", :force => true do |t|
+    t.string   "title"
+    t.text     "desc"
+    t.string   "meta_key"
+    t.string   "meta_desc"
+    t.integer  "pages_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
