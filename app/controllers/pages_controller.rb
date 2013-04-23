fae6b9605733e8ecbb8e49ece937a44bccd41893
clauz <clauz@hu3.ru>
2013-04-23 17:20:42 +0400

    + теперь можно верстать страницы либо хамлом либо html с любыми отступами

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,11 @@ class PagesController < ApplicationController
     @page = Page.find_by_path(params[:id])
     @metakey = @page.meta_key
     @metadesc = @page.meta_desc
+
+    begin
+      @rendered = Haml::Engine.new(@page.text).render
+    rescue Haml::SyntaxError
+      @rendered = Haml::Engine.new(@page.text.gsub(/\s{2,}/, '')).render
+    end
   end
 end
