794ed9c2e02c327277810d72d8a77184dc336a05
vened <maxstbn@gmail.com>
2013-06-03 19:27:25 +0400

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+worker_processes 4
+working_directory "/home/chuck/www/production"
+
+listen "/home/chuck/www/production/tmp/sockets/unicorn.sock", :backlog => 64
+pid "/home/chuck/www/production/tmp/pids/unicorn.pid"
+
+timeout 30
+
+stderr_path "/home/chuck/www/production/log/unicorn.stderr.log"
+stdout_path "/home/chuck/www/production/log/unicorn.stdout.log"
+
+preload_app true
+check_client_connection false
+
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
