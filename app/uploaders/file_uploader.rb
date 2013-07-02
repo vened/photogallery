b05c370743025b4f5f49a1825362326bdae20fb3
vened <maxstbn@gmail.com>
2013-07-02 08:50:49 +0400

    rm uploads

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -8,7 +8,7 @@ class FileUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "upload/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :mini do
