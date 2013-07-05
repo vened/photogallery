d9a5c31e906be5881c1214ddd2375ecf7fb55f55
chuck <maxstbn@gmail.com>
2013-07-05 12:05:57 +0400

    fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ActiveRecord::Base
   validates_length_of :username, :in => 2..50, :message => "Имя должно быть длинной не менее 2 и не более 50 символов"
   validates :email, :format => {:with => /[A-Za-z0-9._%+-]+@([A-Za-z0-9-]+\.)+([A-Za-z0-9]{2,4}|museum)/,
                                 :message => "Не правильно указан адрес e-mail"}
-  validates_length_of :phone, :in => 9..15, :message => "Не указан телефон"
+  validates_length_of :phone, :in => 5..15, :message => "Не указан телефон"
   validates_length_of :address, :in => 5..300, :message => "Не указан адрес"
   validates_length_of :date, :in => 5..300, :message => "Не указана дата доставки"
   validates_length_of :first_time, :in => 2..100, :message => "Не указано время от"
