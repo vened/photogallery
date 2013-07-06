e651a7c42379559e83d16b581cfd16aed4aa0c51
chuck <maxstbn@gmail.com>
2013-07-06 13:15:39 +0400

    orders index

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -2,6 +2,22 @@
 ActiveAdmin.register Order do
 
   menu :label => "Заказы"
+  config.batch_actions = false
+
+  filter :username, :label => "asdcd"
+
+
+  index do
+    render "index"
+  end
+
+
+  #index do
+  #  column "Номер заказа", :id, :sortable => :id
+  #  column "Имя", :username, :sortable => false
+  #  column "Фамилия", :surname, :sortable => false
+  #  actions :defaults => true
+  #end
 
 
 end
