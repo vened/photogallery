e81c6a60a3129852a647dfd222d6d29513a93c2b
chuck <maxstbn@gmail.com>
2013-07-09 11:18:35 +0400

    add cart cell

--- app/cells/cart_cell.rb
+++ app/cells/cart_cell.rb
@@ -7,6 +7,16 @@ class CartCell < Cell::Rails
     render
   end
 
+  def mini_cart
+    @cart = find_cart
+    @prices = 0
+    for item in @cart.items
+      product = Product.find(item[0])
+      @prices = @prices + product.price.to_i * item[1]
+    end
+    render
+  end
+
   private
 
   def find_cart
