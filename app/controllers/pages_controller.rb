c2784d3bba7e9028fc151d0c3cc487f054c08d60
chuck <maxstbn@gmail.com>
2013-09-16 23:07:17 +0400

    chakra design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,13 +7,13 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find_by_path(params[:id])
-    if @page.meta_title.nil?
-      @meta_title = @page.title
-    else
-      @meta_title = @page.meta_title
-    end
-    @metakey = @page.meta_key
-    @metadesc = @page.meta_desc
+    #if @page.meta_title.nil?
+    #  @meta_title = @page.title
+    #else
+    #  @meta_title = @page.meta_title
+    #end
+    #@metakey = @page.meta_key
+    #@metadesc = @page.meta_desc
   end
 
 end
